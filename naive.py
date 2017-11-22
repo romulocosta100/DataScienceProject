6935f5a61e26c99bc2516297eb58acf0fc3c95ba
Rômulo <romulocosta100@gmail.com>
2017-11-22 11:29:30 -0200

    versao atualizada do gabarito

--- naive.py
+++ naive.py
@@ -2,46 +2,47 @@
 # -*- coding: utf-8 -*-
 from unicodedata import normalize
 import json
-import numpy as np
+#import numpy as np
 import re
 import csv
+import editdistance
 
-def levenshtein(source, target):
-	if len(source) < len(target):
-		return levenshtein(target, source)
+# def levenshtein(source, target):
+# 	if len(source) < len(target):
+# 		return levenshtein(target, source)
 
-	# So now we have len(source) >= len(target).
-	if len(target) == 0:
-		return len(source)
+# 	# So now we have len(source) >= len(target).
+# 	if len(target) == 0:
+# 		return len(source)
 
-	# We call tuple() to force strings to be used as sequences
-	# ('c', 'a', 't', 's') - numpy uses them as values by default.
-	source = np.array(tuple(source))
-	target = np.array(tuple(target))
+# 	# We call tuple() to force strings to be used as sequences
+# 	# ('c', 'a', 't', 's') - numpy uses them as values by default.
+# 	source = np.array(tuple(source))
+# 	target = np.array(tuple(target))
 
-	# We use a dynamic programming algorithm, but with the
-	# added optimization that we only need the last two rows
-	# of the matrix.
-	previous_row = np.arange(target.size + 1)
-	for s in source:
-		# Insertion (target grows longer than source):
-		current_row = previous_row + 1
+# 	# We use a dynamic programming algorithm, but with the
+# 	# added optimization that we only need the last two rows
+# 	# of the matrix.
+# 	previous_row = np.arange(target.size + 1)
+# 	for s in source:
+# 		# Insertion (target grows longer than source):
+# 		current_row = previous_row + 1
 
-		# Substitution or matching:
-		# Target and source items are aligned, and either
-		# are different (cost of 1), or are the same (cost of 0).
-		current_row[1:] = np.minimum(
-				current_row[1:],
-				np.add(previous_row[:-1], target != s))
+# 		# Substitution or matching:
+# 		# Target and source items are aligned, and either
+# 		# are different (cost of 1), or are the same (cost of 0).
+# 		current_row[1:] = np.minimum(
+# 				current_row[1:],
+# 				np.add(previous_row[:-1], target != s))
 
-		# Deletion (target grows shorter than source):
-		current_row[1:] = np.minimum(
-				current_row[1:],
-				current_row[0:-1] + 1)
+# 		# Deletion (target grows shorter than source):
+# 		current_row[1:] = np.minimum(
+# 				current_row[1:],
+# 				current_row[0:-1] + 1)
 
-		previous_row = current_row
+# 		previous_row = current_row
 
-	return previous_row[-1]
+# 	return previous_row[-1]
 
 def remover_acentos(txt, codif='utf-8'):
 	return normalize('NFKD', txt).encode('ASCII','ignore')
@@ -104,29 +105,33 @@ if __name__ == "__main__":
 	for vaga1 in dataSet:
 		textoVaga1 = toClean(vaga1['title']+" "+vaga1['description'])
 		
+		
 		grafo[int(vaga1['id'])] = set()
 
+
 		if vaga1['id'] not in setVisitados:
+			print vaga1['id'], "\n"
 			for vaga2 in dataSet:
 				if vaga1['id']!= vaga2['id']:
 					textoVaga2 = toClean(vaga2['title']+" "+vaga2['description'])
 
-					d = levenshtein(textoVaga1,textoVaga2)
+					#d = levenshtein(textoVaga1,textoVaga2)
+					d = int(editdistance.eval(textoVaga1,textoVaga2))
 					valor = 1 - d/float(max( len(textoVaga2),len(textoVaga1)) )
 					if valor==1.0:
 						setVisitados.add(vaga1['id'])
-						setVisitados.add(vaga2['id'])
-					if valor>0.6:
+					 	setVisitados.add(vaga2['id'])
+					if valor>0.68:
 						grafo[int(vaga1['id'])].add(int(vaga2['id']))
-
-						print "distancia entre ",vaga1['id']," ",vaga2['id'], 'd ', valor
+						#print "distancia entre ",vaga1['id']," ",vaga2['id'], 'd ', valor
 
 	itemGrupo = dfs_paths(grafo)
 
-	with open("gabarito.csv", "wb") as csv_file:
+	with open("gabaritoNEW.csv", "wb") as csv_file:
 		writer = csv.writer(csv_file, delimiter=',')
 		for vaga in dataSet:
 			ID = int(vaga['id'])
 			# print ID," ",itemGrupo[ID], "\n"
 			writer.writerow([ID, itemGrupo[ID]])
+
 
